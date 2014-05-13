5a5d023ff5a0416219ac42b6d895202a15a5b7d6
George Rogers <geo@debian>
2014-05-13 11:33:21 -0500

    foo

--- test/bton_test.exs
+++ test/bton_test.exs
@@ -1,10 +1,14 @@
 defmodule BtonTest do
   use ExUnit.Case
 
+  def rs(v) do
+    assert Bton.read(Bton.print(v)) === v
   test "numbers" do
     assert Bton.parse("12") == 12
   end
   test "strings" do
     assert Bton.parse("5\"hello") == "hello"
+  end
+  test "read show" do
   end
 end
